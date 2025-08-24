6fc48583ddedbdc24945c520b7dffa95928816c3
Digital2025-byte <mohamedyasserdigital@gmail.com>
2025-08-24 11:11:47 +0300

    df

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,15 +23,14 @@ export const metadata = {
 
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },   // ICO fallback
-      { url: '/favicon.ico', sizes: '16x16', type: 'image/png' },   // 16x16 PNG
-      { url: '/favicon.ico', sizes: '32x32', type: 'image/png' },   // 32x32 PNG
-      { url: '/favicon.ico', type: 'image/svg+xml' },  // SVG format
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/icon.svg', type: 'image/svg+xml' }, // optional
     ],
-    apple: [{ url: '/favicon.ico', sizes: '180x180' }],  // Apple icon
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
     other: [{ rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#0B4572' }],
   },
-
 };
 
 export default function RootLayout({ children }) {
@@ -45,6 +44,10 @@ export default function RootLayout({ children }) {
         />
 
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+
+
+
         <style>{`
           body, h1, p, h2, h3, h4, h5, h6, div, span {
             font-family: 'Montserrat', sans-serif !important;
